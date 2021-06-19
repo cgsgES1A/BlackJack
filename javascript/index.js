e963ff042966f8fcbfba7c0ed23f6aaa07760b63
Львова Диана <cgsg123@ad.pml30.ru>
2021-06-19 15:33:43 +0300

    Add cookies processing

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,9 @@
 import _ from 'lodash'
 import * as $ from 'jquery';
+import { io, Socket } from 'socket.io-client';
+const Cookies = require('js-cookie');
+
+let userName = null;
 
 function logupUser() {
     let name = $("#input_name").value;
@@ -10,6 +14,8 @@ function logupUser() {
             method: 'POST',
             body: JSON.stringify({ name: name, password: password }),
         });
+        Cookies.set('userName', name);
+        Cookies.get('userPassword', password);
     }
     else
         alert("Input user data to logup");
@@ -21,31 +27,74 @@ function loginUser() {
     let isOkName = true, isOkPassword = true;
 
     if (!name || name.length == 0)
-        isOkName = false;
+        if (!Cookies.get('userName'))
+            isOkName = false;
     if (!password || password.length == 0)
-        isOkPassword = false;
+        if (!Cookies.get('userPassword'))
+            isOkPassword = false;
 
     if (!isOkName || !isOkPassword)
         alert("Input user data to login");
-    else
+    else {
         fetch('/login', {
             method: 'POST',
             body: JSON.stringify({ name: name, password: password }),
         });
+        userName = name;
+    }
 }
 
-function logupRoom() {
-    let roomId = $("#input_room_id").value;
+let roomId = null;
 
-    if (!roomId || roomId.length == 0)
-        alert("Input room data to logup new room");
+function logupRoom() {
+    roomId = 0;
+
+    fetch('/create_room', { method: 'GET' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Response status is: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(text => {
+            let tmpRoomId = text;
+
+            if (!tmpRoomId)
+                alert("Error in room logup");
+            else {
+                roomId = tmpRoomId;
+                roomSession();
+            }
+        });
 }
 
 function loginRoom() {
-    let roomId = $("#input_room_id").value;
+    let tmpRoomId = $("#input_room_id").value;
 
-    if (!roomId || roomId.length == 0)
+    if (!tmpRoomId || tmpRoomId.length == 0)
         alert("Input room data to log in room");
+    else {
+        roomId = tmpRoomId;
+        roomSession();
+    }
+}
+
+function roomSession() {
+    if (!roomId || roomId.length == 0) {
+        alert("Error in room connection (wrong id)");
+        return;
+    }
+    if (!Cookies.get('userName'))
+        alert("You need to login before room connecting");
+    var socket = io({ query: { name: Cookies.get('userName'), room_id: roomId } });
+    socket.on("entersuccessful", (result) => {
+        let enterFlag = parseInt(result);
+
+        if (!enterFlag)
+            socket.disconnect();
+        else
+            ;///smthg
+    });
 }
 
 function onLoad() {
