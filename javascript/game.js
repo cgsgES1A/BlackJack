d4ea12f7ccf30bf5c8c74e1d66f25c55962d2065
ES1A <kat22@mail.ru>
2021-06-24 12:54:50 +0300

    WIP

--- javascript/game.js
+++ javascript/game.js
@@ -117,10 +117,9 @@ export function user_finish_step() {
 
 export function start_game(value1, value2, players, nicknames) {
     let text;
-    alert(nicknames);
     for (var i = 1; i < players + 1; i++) {
         text = $(`<p>${nicknames[i - 1]}</p>`);
-        text.css({ 'position': "absolute", 'color': 'white', 'font-size': '20', 'top': `${5 + 15 * (i - 1)}%`, 'left': "0%" });
+        text.css({ 'position': "absolute", 'color': 'white', 'font-size': '20', 'top': `${8.5 + 15 * (i - 1)}%`, 'left': "0%" });
         $("#axis").append(text);
     }
     text = $("<div><p>Dealer</p></div>");
